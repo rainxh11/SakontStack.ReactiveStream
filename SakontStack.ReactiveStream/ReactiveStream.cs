122438c8512e4df8868bae0737cd33958fd5e933
Ahmed Chakhoum <rainxh11@gmail.com>
2023-08-19 19:47:32 +0100

    + Fixed bugs, added speed limit

--- SakontStack.ReactiveStream/ReactiveStream.cs
+++ SakontStack.ReactiveStream/ReactiveStream.cs
@@ -1,165 +1,194 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
 using System.Threading;
 using static SakontStack.ReactiveStream.ReactiveStream;
 
 namespace SakontStack.ReactiveStream;
 
-
-
 public class ReactiveStream : Stream, IObservable<StreamProgress>
 {
-    private readonly Stream                      _stream;
-    private readonly IProgress<StreamProgress>   _progress;
+    private readonly Stream _stream;
+    private readonly IProgress<StreamProgress>? _progress;
+    private readonly long? _totalLength;
     private readonly IObservable<StreamProgress> _observable;
-    private readonly StreamOptions               _options = new();
-    private event EventHandler<StreamProgress>?  OnProgress;
+    private readonly StreamOptions _options = new();
+    private event EventHandler<StreamProgress>? OnProgress;
+    private ConcurrentBag<TimestampedDelta> _deltas = new();
+
+    private SemaphoreSlim _readLock  = new(1, 1);
+    private SemaphoreSlim _writeLock = new(1, 1);
 
     public class StreamOptions
     {
-        public TimeSpan ReportInterval { get; set; } = TimeSpan.Zero;
+        public long? WriteSpeedLimit { get; set; } = null;
+        public long? ReadSpeedLimit { get; set; } = null;
     }
 
     public enum ProgressType
     {
         Read,
-        Write,
+        Write
     }
+
+    public record struct TimestampedDelta(long Delta, ProgressType Type, DateTimeOffset Timestamp);
+
     public class StreamProgress
     {
-        public ProgressType Type       { get; init; }
-        public long?        TotalBytes { get; init; }
-        public long         Bytes      { get; init; }
-        public long         Delta      { get; init; }
+        public ProgressType Type { get; init; }
+        public long? TotalBytes { get; init; }
+        public long Bytes { get; init; }
+        public long Delta { get; init; }
+        public long BytesPerSecond { get; init; }
+        public long? SpeedLimit { get; init; }
 
         public double Percentage => TotalBytes is null or 0
                                         ? double.NegativeInfinity
                                         : Bytes * 100 / (double)TotalBytes;
     }
 
+    public void ModifyOptions(Action<StreamOptions> configureOptions) => configureOptions(_options);
 
-    public ReactiveStream(Stream stream, IProgress<StreamProgress> progress = null, Action<StreamOptions> configureStream = null)
+
+    public ReactiveStream(Stream stream, IProgress<StreamProgress>? progress = null,
+                          Action<StreamOptions>? configureStream = null,
+                          long? totalLength = 0)
     {
-        _stream   = stream;
+        _stream = stream;
         _progress = progress;
+        _totalLength = totalLength;
         configureStream?.Invoke(_options);
 
+
         _observable = Observable
-           .FromEvent<EventHandler<StreamProgress>, StreamProgress>(h => OnProgress += h,
-                                                                    h => OnProgress -= h);
+                     .FromEventPattern<EventHandler<StreamProgress>, StreamProgress>(h => OnProgress += h,
+                          h => OnProgress -= h)
+                     .Select(x => x.EventArgs);
     }
 
+    private TimeSpan ReachedLimit(ProgressType type)
+    {
+        switch (type)
+        {
+            case ProgressType.Read when _options.ReadSpeedLimit is null:
+            case ProgressType.Write when _options.WriteSpeedLimit is null:
+                return TimeSpan.Zero;
+            default:
+                {
+                    var now = DateTimeOffset.UtcNow;
+                    var lastSecond = _deltas
+                                    .Where(x => now - x.Timestamp <= TimeSpan.FromSeconds(1)
+                                                && x.Type == type).ToList();
+
+                    var deltas = lastSecond.Sum(x => x.Delta);
+                    var diff = now - (lastSecond.Count > 0
+                                          ? lastSecond.MinBy(x => x.Timestamp).Timestamp
+                                          : now);
+                    var ct = new CancellationTokenSource(diff);
+
+                    var limit = type == ProgressType.Read ? _options.ReadSpeedLimit : _options.WriteSpeedLimit;
+                    return limit <= deltas ? diff : TimeSpan.Zero;
+                }
+        }
+    }
 
     public override int Read(byte[] buffer, int offset, int count)
     {
-        int totalBytesRead = 0;
+        var limit = ReachedLimit(ProgressType.Read);
+        if (limit > TimeSpan.Zero) _readLock.Wait(limit);
+
+        var totalBytesRead = 0;
         while (totalBytesRead < count)
         {
-            int bytesRead = _stream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+            var bytesRead = _stream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
             if (bytesRead == 0) break;
             totalBytesRead += bytesRead;
         }
+
+        var now = DateTimeOffset.Now;
         OnProgress?.Invoke(this, new StreamProgress()
-                                 {
-                                     TotalBytes = this.Length,
-                                     Delta      = totalBytesRead,
-                                     Bytes      = offset + totalBytesRead,
-                                     Type       = ProgressType.Read
-                                 });
+        {
+            TotalBytes = Length,
+            Delta = totalBytesRead,
+            Bytes = Position,
+            Type = ProgressType.Read,
+            BytesPerSecond = _deltas
+                                                     .Where(x => now - x.Timestamp <= TimeSpan.FromSeconds(1)
+                                                                 && x.Type == ProgressType.Read)
+                                                     .Sum(x => x.Delta),
+            SpeedLimit = _options.ReadSpeedLimit
+        });
         return totalBytesRead;
     }
 
-    public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-    {
-        var result = await base.ReadAsync(buffer, offset, count, cancellationToken);
-        if (!cancellationToken.IsCancellationRequested) OnProgress?.Invoke(this, new StreamProgress()
-                                                                               {
-                                                                                   TotalBytes = this.Length,
-                                                                                   Delta      = result,
-                                                                                   Bytes      = offset + result,
-                                                                                   Type       = ProgressType.Read
-                                                                               });
-        return result;
-    }
-
-    public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = new CancellationToken())
-    {
-        var position = Position;
-        var result   = await base.ReadAsync(buffer, cancellationToken);
-        if (!cancellationToken.IsCancellationRequested) OnProgress?.Invoke(this, new StreamProgress()
-                                                                               {
-                                                                                   TotalBytes = this.Length,
-                                                                                   Delta      = result,
-                                                                                   Bytes      = position + result,
-                                                                                   Type       = ProgressType.Read
-                                                                               });
-        return result;
-    }
 
     public override void Write(byte[] buffer, int offset, int count)
     {
+        var limit = ReachedLimit(ProgressType.Write);
+        if (limit > TimeSpan.Zero) _writeLock.Wait(limit);
+
         _stream.Write(buffer, offset, count);
+
+        var now = DateTimeOffset.Now;
         OnProgress?.Invoke(this, new StreamProgress()
-                                 {
-                                     TotalBytes = this.Length,
-                                     Delta      = count,
-                                     Bytes      = offset + count,
-                                     Type       = ProgressType.Write
-                                 });
+        {
+            TotalBytes = Length,
+            Delta = count,
+            Bytes = Position,
+            Type = ProgressType.Write,
+            BytesPerSecond = _deltas
+                                                     .Where(x => now - x.Timestamp <= TimeSpan.FromSeconds(1)
+                                                                 && x.Type == ProgressType.Write)
+                                                     .Sum(x => x.Delta),
+            SpeedLimit = _options.WriteSpeedLimit
+        });
     }
 
-    public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    public override void Flush()
     {
-        await base.WriteAsync(buffer, offset, count, cancellationToken);
-        if (!cancellationToken.IsCancellationRequested) OnProgress?.Invoke(this, new StreamProgress()
-                                                                               {
-                                                                                   TotalBytes = this.Length,
-                                                                                   Delta      = count,
-                                                                                   Bytes      = offset + count,
-                                                                                   Type       = ProgressType.Read
-                                                                               });
+        _stream.Flush();
     }
 
-    public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = new CancellationToken())
+    public override long Seek(long offset, SeekOrigin origin)
     {
-        var position = Position;
-        await base.WriteAsync(buffer, cancellationToken);
-        if (!cancellationToken.IsCancellationRequested) OnProgress?.Invoke(this, new StreamProgress()
-                                                                               {
-                                                                                   TotalBytes = this.Length,
-                                                                                   Delta = buffer.Length,
-                                                                                   Bytes = position + buffer.Length,
-                                                                                   Type = ProgressType.Read
-                                                                               });
+        return _stream.Seek(offset, origin);
     }
 
-    public override void Flush()                                   => _stream.Flush();
-    public override long Seek(long      offset, SeekOrigin origin) => _stream.Seek(offset, origin);
-    public override void SetLength(long value) => _stream.SetLength(value);
+    public override void SetLength(long value)
+    {
+        _stream.SetLength(value);
+    }
 
 
-
-    public override bool CanRead  => _stream.CanRead;
-    public override bool CanSeek  => _stream.CanSeek;
+    public override bool CanRead => _stream.CanRead;
+    public override bool CanSeek => _stream.CanSeek;
     public override bool CanWrite => _stream.CanWrite;
-    public override long Length   => _stream.Length;
+    public override long Length => _totalLength ?? _stream.Length;
+
     public override long Position
     {
         get => _stream.Position;
         set => _stream.Position = value;
     }
 
-    private IObservable<StreamProgress> ApplyOptions(IObservable<StreamProgress> observable)
-    {
-        return _options?.ReportInterval > TimeSpan.Zero ? _observable.Sample(_options.ReportInterval) : observable;
-    }
     public IDisposable Subscribe(IObserver<StreamProgress> observer)
     {
-        return ApplyOptions(_observable)
-              .Do(x => _progress?.Report(x))
-              .Subscribe(observer);
+        return _observable.Do(p => _deltas.Add(new TimestampedDelta(p.Delta, p.Type, DateTimeOffset.Now)))
+                          .Subscribe(onNext: x =>
+                          {
+                              _progress?.Report(x);
+                              observer.OnNext(x);
+                          },
+                                     onError: observer.OnError,
+                                     onCompleted: observer.OnCompleted);
     }
+
 
 }
